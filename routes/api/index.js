fbec729e37b1192473148c52decb4495558d6a26
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-02-14 13:23:11 -0500

    Build: Finished first run on routes folder with all files coded

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const thoughtsRoutes = require('./thoughtsRoutes');
+const userRoutes = require('./userRoutes');
+const reaction = require('./reaction');
+
+router.use('/thoughts', thoughtsRoutes);
+router.use('./users', userRoutes);
+router.use('/reactions', reaction);
+
+module.export = router;
